6fbd4d7a8be4998d27f056d18d76c3f6366db7d9
Raunak Redkar <raunak.redkar@gmail.com>
2023-10-13 13:02:38 +0200

    im bad at programming

--- src/ast.rs
+++ src/ast.rs
@@ -11,6 +11,43 @@ pub enum Condition {
     NotEqual,
 }
 
+pub enum Alone {
+    Int(i32);
+    VarName(i32);
+    Paren(Expr);
+}
+
+pub enum AB {
+    Mul {
+        ab: AB,
+        alone: Alone, 
+    },
+    Div {
+        ab: AB,
+        alone: Alone, 
+    },
+    alone(Alone);
+}
+
+pub enum Expr {
+    Plus {
+        expr: Expr,
+        ab: AB,
+    },
+    Minus {
+        expr: Expr,
+        ab: AB,
+    },
+    ab(AB), 
+}
+
+pub enum BoolExpr {
+    cond: Condition,
+    expr1: Option<Expr>,
+    expr2: Option<Expr>, 
+}
+
+
 pub enum Node_Type {
     Body,
     Assign,
@@ -18,12 +55,10 @@ pub enum Node_Type {
     While,
     If, 
     Else,
-    BoolExpr {
-        cond: Condition, 
-    }
-    Expr,
-    AB,
-    Alone, 
+    BoolExpr(BoolExpr),
+    Expr(Expr),
+    AB(AB),
+    Alone(Alone);, 
     Death,
 }
 
@@ -42,21 +77,27 @@ Token {token: TokenType::Die, ..} => {
 
 // I think this function can be private
 fn ast_if(node: &mut Node, iter: &mut Peekable<Iter<Token>>) {
-    iter.next();
-    match iter.peek() {
-        Token {token: TokenType::True, ..} => {
-            node.kids.push(Node {tp: Node_Type::BoolExpr{cond: Condition::True}, kids: vec![]});
-        },
-        Token {token: TokenType::False, ..} => {
-            node.kids.push(Node {tp: Node_Type::BoolExpr{cond: Condition::False}, kids: vec![]});
-        },
-        Token{token: TokenType::NotEqual, ..} => {
-            node.kids.push(Node{tp: Node_Type::BookExpr{cond: Condition::NotEqual}, kids: vec![]});
-        }
-        _ => {
-            // is an expression
-        }
-    }
+
+}
+
+fn ast_expr(&mut Node, iter: &mut Peekable<Iter<Token>>) -> Node_Type::Expr {
+
+}
+
+fn ast_boolexpr (node: &mut Node, iter: &mut Peekable<Iter<Token>>) -> Node_Type::BoolExpr {
+
+}
+
+fn ast_AB (node: &mut Node, iter: &mut Peekable<Iter<Token>>) {
+    todo!();
+}
+
+fn ast_alone (node: &mut Node, iter: &mut Peekable<Iter<Token>>) {
+    todo!();
+}
+
+fn ast_expr (node: &mut Node, iter: &mut Peekable<Iter<Token>>) {
+    todo!();
 }
 
 fn ast_varinit (node: &mut Node, iter: &mut Peekable<Iter<Token>>) {
@@ -75,8 +116,15 @@ fn ast_while (node: &mut Node, iter: &mut Peekable<Iter<Token>>) {
     todo!();
 }
 
+fn ast_body (node: &mut Node, iter: &mut Peekable<Iter<Token>>) {
+    todo!();
+}
+
+
 pub fn make_ast(tokens: Vec<Token>) -> Node {
     let mut start_node = Node {tp: Body, kids: Vec![] };
     let mut iter = tokens.iter().peekable();    
+    ast_body(&mut start_node, &mut iter);
+    return start_node;
 }
 
